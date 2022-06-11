ea5c93c8d0aa9232f242b524e5bb1d79ca62b589
dwellrr <guardianofthewishingwell@gmail.com>
2022-06-11 20:51:19 +0200

    Added a player class, to be implemented

--- fighter.py
+++ fighter.py
@@ -8,9 +8,32 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Rye Fighter")
 
+class player:
+    def __init__(self, name):
+        hitbox = pygame.Rect(WIDTH/2, HEIGHT/2, 50, 50)
+        sprite_name = name
+        current_action = 'idle'
+        frame_queue = []
+        health = 100
+        dmg = 0
+        cooldown = 0
+
+    def handle_input():
+        return #TODO
+    def handle_dmg(dmg):
+        return #TODO
+    #def handle_animations()
+    def update(self):
+        self.handle_input()
+        if self.dmg != 0:
+            self.handle_dmg()
+        #self.handle_animations()
+
+
 
 def draw_window():
     pygame.draw.rect(WIN, (0, 0, 0), (pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)))
+    pygame.display.update()
 
 def main():
     clock = pygame.time.Clock()
